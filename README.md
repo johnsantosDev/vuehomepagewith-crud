791902b9f6aec0ee5a7fc4510eee78a44ec9711b
johnsantosDev <johnndiwayesusantos@gmail.com>
2023-04-04 11:04:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+https://github.com/johnsantosDev/vuehomepagewith-crud
+
+
+clone or download zip of this repo
+cd to the unpacked folder
+run npm install
+run http server of your choice (eg whs or SimpleHTTPServer)
+for me, I am runnig using python : python -m SimpleHTTPServer 8000
+
+
---
+++ src/js/index.js
@@ -0,0 +1,108 @@
+var products = [
+  {id: 1, name: 'Angular',  price: 100},
+  {id: 2, name: 'Ember', price: 100},
+  {id: 3, name: 'React', price: 100}
+];
+
+function findProduct (productId) {
+  return products[findProductKey(productId)];
+};
+
+function findProductKey (productId) {
+  for (var key = 0; key < products.length; key++) {
+    if (products[key].id == productId) {
+      return key;
+    }
+  }
+};
+
+var List = Vue.extend({
+  template: '#product-list',
+  data: function () {
+    return {products: products, searchKey: ''};
+  },
+  computed : {
+    filteredProducts: function () {
+    var self = this;
+    console.log()
+    return self.products.filter(function (product) {
+      return product.name.indexOf(self.searchKey) !== -1
+    })
+  }
+}
+});
+
+var Product = Vue.extend({
+  template: '#product',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  }
+});
+
+var ProductEdit = Vue.extend({
+  template: '#product-edit',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  },
+  methods: {
+    updateProduct: function () {
+      var product = this.product;
+      products[findProductKey(product.id)] = {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        price: product.price
+      };
+      router.push('/');
+    }
+  }
+});
+
+var ProductDelete = Vue.extend({
+  template: '#product-delete',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  },
+  methods: {
+    deleteProduct: function () {
+      products.splice(findProductKey(this.$route.params.product_id), 1);
+      router.push('/');
+    }
+  }
+});
+Vue.component('homepage', {
+  template: '#homepage'
+});
+var AddProduct = Vue.extend({
+  template: '#add-product',
+  data: function () {
+    return {product: {name: '', price: ''}
+    }
+  },
+  methods: {
+    createProduct: function() {
+      var product = this.product;
+      products.push({
+        id: Math.random().toString().split('.')[1],
+        name: product.name,
+        price: product.price
+      });
+      router.push('/');
+    }
+  }
+});
+
+var router = new VueRouter({
+  routes: [{path: '/', component: List},
+    {path: '/product/:product_id', component: Product, name: 'product'},
+    {path: '/add-product', component: AddProduct},
+    {path: '/product/:product_id/edit', component: ProductEdit, name: 'product-edit'},
+  {path:   '/product/:product_id/delete', component: ProductDelete, name: 'product-delete'},
+  { path: '/new-page', component: Vue.component('homepage') } 
+]});
+
+new Vue({
+  el: '#app',
+  router: router,
+  template: '<router-view></router-view>'
+});
